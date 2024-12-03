b963c6964161928d0cd497ed751df20ecbfca8fd
Márton Balogh-Bodor <marton2@gmail.com>
2024-12-03 12:08:24 +0100

    Day 03, Question 02, Try 01

--- 03/main.go
+++ 03/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"sort"
 	"strconv"
 )
 
@@ -12,25 +13,63 @@ func main() {
 	file, _ := os.Open("input.txt")
 	defer file.Close()
 
-	var regex = regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
-	var numbers = make([][]int, 0)
+	var regex_muls = regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
+	var regex_dos = regexp.MustCompile(`do\(\)`)
+	var regex_donts = regexp.MustCompile(`don\'t\(\)`)
+	var instructions = make([][]int, 0)
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
-		matches := regex.FindAllStringSubmatch(line, -1)
+		muls := regex_muls.FindAllStringSubmatch(line, -1)
+		muli := regex_muls.FindAllStringSubmatchIndex(line, -1)
 
-		for _, match := range matches {
+		dos := regex_dos.FindAllStringSubmatch(line, -1)
+		doi := regex_dos.FindAllStringSubmatchIndex(line, -1)
+
+		donts := regex_donts.FindAllStringSubmatch(line, -1)
+		donti := regex_donts.FindAllStringSubmatchIndex(line, -1)
+
+		for index, match := range muls {
 			num1, _ := strconv.Atoi(match[1])
 			num2, _ := strconv.Atoi(match[2])
-			numbers = append(numbers, []int{num1, num2})
+			instructions = append(instructions, []int{muli[index][0], 0, num1, num2})
+		}
+
+		for index, _ := range dos {
+			instructions = append(instructions, []int{doi[index][0], 1, 0, 0})
+		}
+
+		for index, _ := range donts {
+			instructions = append(instructions, []int{donti[index][0], 2, 0, 0})
 		}
 	}
 
+	sort.Slice(instructions, func(i, j int) bool {
+		return instructions[i][0] < instructions[j][0]
+	})
+
 	question1 := 0
-	for _, nums := range numbers {
-		question1 += nums[0] * nums[1]
+	question2 := 0
+	enabled := true
+	for _, nums := range instructions {
+		if nums[1] == 0 {
+			question1 += nums[2] * nums[3]
+		}
+
+		if nums[1] == 0 && enabled {
+			question2 += nums[2] * nums[3]
+		}
+
+		if nums[1] == 1 {
+			enabled = true
+		}
+
+		if nums[1] == 2 {
+			enabled = false
+		}
 	}
 
 	fmt.Println("Question 1:", question1)
+	fmt.Println("Question 2:", question2)
 }

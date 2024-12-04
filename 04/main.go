225418a49ad03382f5ea2429921e326b7bae6b8d
Márton Balogh-Bodor <marton2@gmail.com>
2024-12-04 17:18:25 +0100

    Day 04, Question 02, Try 01

--- 04/main.go
+++ 04/main.go
@@ -22,22 +22,38 @@ func main() {
 		field = append(field, array)
 	}
 
-	needle := "XMAS"
 	question1 := 0
+	/*for i := 0; i < len(field); i++ {
+		for j := 0; j < len(field[i]); j++ {
+			question1 += search(i, j, 0, -1, 0, "XMAS", field)  // up
+			question1 += search(i, j, 0, -1, 1, "XMAS", field)  // up right
+			question1 += search(i, j, 0, 0, 1, "XMAS", field)   // right
+			question1 += search(i, j, 0, 1, 1, "XMAS", field)   // down right
+			question1 += search(i, j, 0, 1, 0, "XMAS", field)   // down
+			question1 += search(i, j, 0, 1, -1, "XMAS", field)  // down left
+			question1 += search(i, j, 0, 0, -1, "XMAS", field)  // left
+			question1 += search(i, j, 0, -1, -1, "XMAS", field) // up left
+		}
+	}*/
+
+	//Two MAS in the shape of an X
+	question2 := 0
 	for i := 0; i < len(field); i++ {
 		for j := 0; j < len(field[i]); j++ {
-			question1 += search(i, j, 0, -1, 0, needle, field)  // up
-			question1 += search(i, j, 0, -1, 1, needle, field)  // up right
-			question1 += search(i, j, 0, 0, 1, needle, field)   // right
-			question1 += search(i, j, 0, 1, 1, needle, field)   // down right
-			question1 += search(i, j, 0, 1, 0, needle, field)   // down
-			question1 += search(i, j, 0, 1, -1, needle, field)  // down left
-			question1 += search(i, j, 0, 0, -1, needle, field)  // left
-			question1 += search(i, j, 0, -1, -1, needle, field) // up left
+			m1 := search(i, j, 0, -1, -1, "AM", field) + search(i, j, 0, 1, 1, "AS", field) //MAS or
+			m2 := search(i, j, 0, -1, -1, "AS", field) + search(i, j, 0, 1, 1, "AM", field) //SAM (left to right)
+
+			m3 := search(i, j, 0, -1, 1, "AM", field) + search(i, j, 0, 1, -1, "AS", field) //MAS or
+			m4 := search(i, j, 0, -1, 1, "AS", field) + search(i, j, 0, 1, -1, "AM", field) //SAM (right to left)
+
+			if m1+m2 == 2 && m3+m4 == 2 {
+				question2++
+			}
 		}
 	}
 
 	println("Question 1:", question1)
+	println("Question 2:", question2)
 }
 
 func search(i int, j int, p int, di int, dj int, needle string, haystack [][]string) int {

c82e8b3575d2825c4af5d47d29bd8d504df68c80
Márton Balogh-Bodor <marton2@gmail.com>
2024-12-06 11:51:13 +0100

    Day 06, Question 02, Try 01

--- 06/main.go
+++ 06/main.go
@@ -19,11 +19,14 @@ func main() {
 	}
 
 	guard := []int{0, 0}
+	orig := []int{0, 0}
 	for col, _ := range fields {
 		for row, _ := range fields[col] {
 			if fields[row][col] == "^" {
 				guard[0] = row
 				guard[1] = col
+				orig[0] = row
+				orig[1] = col
 			}
 		}
 	}
@@ -63,7 +66,22 @@ func main() {
 		}
 	}
 
+	question2 := 0
+	for y, row := range fields {
+		for x, _ := range row {
+			if fields[y][x] == "#" || fields[y][x] == "^" {
+				continue
+			}
+
+			fields[y][x] = "#"
+			guard = orig
+			question2 += checkLoop(fields, guard, []int{-1, 0})
+			fields[y][x] = "."
+		}
+	}
+
 	println("Question 1:", len(distinct))
+	println("Question 2:", question2)
 }
 
 func turn(direction []int) ([]int, error) {
@@ -78,4 +96,26 @@ func turn(direction []int) ([]int, error) {
 	}
 
 	return nil, errors.New("Invalid direction")
+}
+
+func checkLoop(fields [][]string, guard []int, direction []int) int {
+	// This seems like a reasonable exit condition to assume that this is a loop. Very demure, very mindful.
+	// There are probably better solutions, I already get the correct answer around 10000 iterations.
+	exit := len(fields) * len(fields[0])
+	for i := 0; i < exit; i++ {
+		next := []int{guard[0] + direction[0], guard[1] + direction[1]}
+
+		if next[0] < 0 || next[0] >= len(fields) || next[1] < 0 || next[1] >= len(fields[next[0]]) {
+			return 0
+		}
+
+		if fields[next[0]][next[1]] == "#" {
+			direction, _ = turn(direction)
+			continue
+		}
+
+		guard = next
+	}
+
+	return 1
 }

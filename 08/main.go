59c83a74b71e592daa9b84fe67075afcc83f0e11
Márton Balogh-Bodor <marton2@gmail.com>
2024-12-08 15:15:55 +0100

    Day 08, Question 02, Try 01

--- 08/main.go
+++ 08/main.go
@@ -38,37 +38,28 @@ func main() {
 		height++
 	}
 
-	antinodes := []coord{}
+	antinodes1 := []coord{}
+	antinodes2 := []coord{}
 	for _, v := range coords {
 		combos := slices.DeleteFunc(combinations.All(v), func(c []coord) bool { return len(c) < 2 })
 		for _, c := range combos {
-			distance := coord{c[1].x - c[0].x, c[1].y - c[0].y}
-			slope := getSlope(c[0], c[1])
-
-			lx := c[0].x - distance.x
-			ly := float64(c[0].y) - (slope * float64(distance.x))
-
-			rx := c[1].x + distance.x
-			ry := float64(c[1].y) + (slope * float64(distance.x))
-
-			if ly != float64(int(ly)) || ry != float64(int(ry)) {
-				fmt.Println("Not an integer")
-				return
+			for _, a := range generateAntinodes(c[0], c[1], width, height, false) {
+				if !slices.Contains(antinodes1, a) {
+					antinodes1 = append(antinodes1, a)
+				}
 			}
 
-			if lx >= 0 && lx < width && ly >= 0 && int(ly) < height && !slices.Contains(antinodes, coord{lx, int(ly)}) {
-				antinodes = append(antinodes, coord{lx, int(ly)})
-			}
-
-			if rx >= 0 && rx < width && ry >= 0 && int(ry) < height && !slices.Contains(antinodes, coord{rx, int(ry)}) {
-				antinodes = append(antinodes, coord{rx, int(ry)})
+			for _, a := range generateAntinodes(c[0], c[1], width, height, true) {
+				if !slices.Contains(antinodes2, a) {
+					antinodes2 = append(antinodes2, a)
+				}
 			}
 		}
 	}
 
 	for i := 0; i < height; i++ {
 		for j := 0; j < width; j++ {
-			if slices.Contains(antinodes, coord{j, i}) {
+			if slices.Contains(antinodes2, coord{j, i}) {
 				fmt.Print("#")
 				continue
 			}
@@ -87,9 +78,50 @@ func main() {
 	}
 
 	fmt.Println()
-	fmt.Println("Question 1:", len(antinodes))
+	fmt.Println("Question 1:", len(antinodes1))
+	fmt.Println("Question 2:", len(antinodes2))
 }
 
+func generateAntinodes(a, b coord, width, height int, harmonics bool) []coord {
+	distance := coord{b.x - a.x, b.y - a.y}
+	slope := getSlope(a, b)
+	results := []coord{}
+
+	px1 := a.x
+	px2 := b.x
+	for true {
+		lx := px1 - distance.x
+		ly := float64(a.y) - (slope * float64(distance.x))
+
+		rx := px2 + distance.x
+		ry := float64(b.y) + (slope * float64(distance.x))
+
+		if ly != float64(int(ly)) || ry != float64(int(ry)) {
+			fmt.Println("Not an integer")
+			return []coord{}
+		}
+
+		added := false
+		if lx >= 0 && lx < width && ly >= 0 && int(ly) < height {
+			results = append(results, coord{lx, int(ly)})
+			added = true
+		}
+
+		if rx >= 0 && rx < width && ry >= 0 && int(ry) < height {
+			results = append(results, coord{rx, int(ry)})
+			added = true
+		}
+
+		if !harmonics || !added {
+			return results
+		}
+
+		px1 = lx
+		px2 = rx
+	}
+
+	return results
+}
 func getSlope(a, b coord) float64 {
 	if b.x == a.x {
 		return 0
